007397edb4a1a05be0eab807203f4cd3b3363b2d
Luis Vela <luismvl114@gmail.com>
2023-07-10 17:06:01 -0500

    click al logo ahora redirecciona a '/'

--- src/js/component/Logo.jsx
+++ src/js/component/Logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Logo = () => {
+const Logo = (props) => {
   return (
     <svg
+      {...props}
       xmlns='http://www.w3.org/2000/svg'
       //   width='2500'
       //   height='1070'
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
+
 import styles from '../../styles/Navbar.module.css'
 
 import Logo from './Logo.jsx'
 import Dropdown from './Dropdown.jsx'
 
 const Navbar = () => {
+  const navigate = useNavigate()
+
   return (
     <div className={styles.navbar}>
-      <Logo />
+      <Logo onClick={() => navigate('/')} />
       <Dropdown />
     </div>
   )
